3a98919bb93b44c5acc45f8abd3ce403802f7cc8
cscottburns <30447990+cscottburns@users.noreply.github.com>
2017-08-04 12:45:06 -0700

    Update intGame.js

--- js/intGame.js
+++ js/intGame.js
@@ -5,8 +5,6 @@ btn.onclick = function() {
     for (var i = 0; i < imgArray.length; i++) {
         var image_x = document.getElementById(imgArray[i]);
         image_x.parentNode.removeChild(image_x);
-        image_x = document.getElementById(imgArray[i]);
-        image_x.parentNode.removeChild(image_x);
     }
     trys = 0;
     match = 0;
